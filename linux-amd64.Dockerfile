a3806616e587f372f5f29d887850835353545ad5
hotio <26902309+hotio@users.noreply.github.com>
2020-09-13 17:18:45 +0200

    rework build

--- linux-amd64.Dockerfile
+++ linux-amd64.Dockerfile
@@ -3,11 +3,11 @@ FROM alpine:3.12 as builder
 # install packages
 RUN apk add --no-cache fuse libattr libstdc++ autoconf automake libtool gettext-dev attr-dev linux-headers make build-base git
 
-ARG MERGERFS_VERSION
+ARG VERSION
 
 # install mergerfs
 RUN git clone -n https://github.com/trapexit/mergerfs.git /mergerfs && cd /mergerfs && \
-    git checkout ${MERGERFS_VERSION} -b hotio && \
+    git checkout ${VERSION} -b hotio && \
     make STATIC=1 LTO=1 && make install
 
 
@@ -22,18 +22,3 @@ RUN apk add --no-cache fuse libattr libstdc++
 COPY --from=builder /usr/local/bin/mergerfs /usr/local/bin/mergerfs
 COPY --from=builder /usr/local/bin/mergerfs-fusermount /usr/local/bin/mergerfs-fusermount
 COPY --from=builder /sbin/mount.mergerfs /sbin/mount.mergerfs
-
-ARG LABEL_CREATED
-LABEL org.opencontainers.image.created=$LABEL_CREATED
-ARG LABEL_TITLE
-LABEL org.opencontainers.image.title=$LABEL_TITLE
-ARG LABEL_REVISION
-LABEL org.opencontainers.image.revision=$LABEL_REVISION
-ARG LABEL_SOURCE
-LABEL org.opencontainers.image.source=$LABEL_SOURCE
-ARG LABEL_VENDOR
-LABEL org.opencontainers.image.vendor=$LABEL_VENDOR
-ARG LABEL_URL
-LABEL org.opencontainers.image.url=$LABEL_URL
-ARG LABEL_VERSION
-LABEL org.opencontainers.image.version=$LABEL_VERSION
--- update.sh
+++ update.sh
@@ -9,9 +9,15 @@ if [[ ${1} == "checkdigests" ]]; then
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "amd64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-amd64.Dockerfile  && echo "${digest}"
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm" and .platform.os == "linux").digest')   && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm-v7.Dockerfile && echo "${digest}"
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm64.Dockerfile  && echo "${digest}"
+elif [[ ${1} == "tests" ]]; then
+    echo "List installed packages..."
+    docker run --rm --entrypoint="" "${2}" apk -vv info | sort
+    echo "Show version info..."
+    echo "mergerfs --version | grep 'mergerfs version:' && exit 0" > "${GITHUB_WORKSPACE}/mergerfstest.sh"
+    docker run --rm --entrypoint="" -v "${GITHUB_WORKSPACE}":"${GITHUB_WORKSPACE}" "${2}" sh "${GITHUB_WORKSPACE}/mergerfstest.sh"
 else
     version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/trapexit/mergerfs/commits/master" | jq -r .sha)
     [[ -z ${version} ]] && exit 1
-    sed -i "s/{MERGERFS_VERSION=[^}]*}/{MERGERFS_VERSION=${version}}/g" .github/workflows/build.yml
+    echo "VERSION=${version}" > VERSION
     echo "##[set-output name=version;]${version}"
 fi
